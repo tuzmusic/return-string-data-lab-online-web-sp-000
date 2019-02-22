a67989c4a3bb13a93d9e20ef45cca00fef6b1212
Jonathan Tuzman <tuzmusic@gmail.com>
2019-02-22 17:40:39 -0500

    remove inventory method

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,5 @@ class Product < ActiveRecord::Base
   has_many :ordered_products
   has_many :orders, :through => :ordered_products
 
-  def inventory
-    Product.where(name: name).count
-  end
 
 end
