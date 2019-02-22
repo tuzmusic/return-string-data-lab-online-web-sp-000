44a6c7db0ac98e9ebc9b25ade1d0def8f90d0fb9
Jonathan Tuzman <tuzmusic@gmail.com>
2019-02-22 12:56:32 -0500

    implement inventory and description endpoints

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,4 +10,14 @@ class ProductsController < ApplicationController
     Product.create(name: params[:name], description: params[:description])
     redirect_to products_path
   end
+
+  def inventory
+    product = Product.find(params[:id])
+    render plain: product.inventory > 0
+  end
+
+  def description
+    product = Product.find(params[:id])
+    render plain: product.description
+  end
 end
