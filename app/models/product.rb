4d6db2eca16342797f1785cc79298b5b8b5605bd
Jonathan Tuzman <tuzmusic@gmail.com>
2019-02-22 14:12:58 -0500

    add description to more info button

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,8 @@ class Product < ActiveRecord::Base
   has_many :ordered_products
   has_many :orders, :through => :ordered_products
 
-  
+  def inventory
+    Product.where(name: name).count
+  end
+
 end
